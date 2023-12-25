1545c7c8406f4e05ea2b47ceda632c99777ff333
ThisIsKhalid <iamkhalid.hs@gmail.com>
2023-12-25 13:30:04 +0600

    set cart into local storage

--- src/Redux/cart/cartSlice.jsx
+++ src/Redux/cart/cartSlice.jsx
@@ -1,18 +1,30 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const cartInitialValue = {
+const cartValue = {
   carts: [],
 };
 
+const loadState = () => {
+  const storedState = localStorage.getItem("cartState");
+
+  return storedState ? JSON.parse(storedState) : cartValue;
+};
+
+const initialState = loadState();
+
 export const cartSlice = createSlice({
   name: "cart",
-  initialState: cartInitialValue,
+  initialState: initialState,
   reducers: {
     addToCart: (state, action) => {
       state.carts = action.payload;
+
+      localStorage.setItem("cartState", JSON.stringify(state));
     },
     addToBuyCart: (state, action) => {
       state.carts.push(action.payload);
+
+      localStorage.setItem("cartState", JSON.stringify(state));
     },
     removeFromCart: (state, action) => {
       const { _id, color, size, quantity } = action.payload;
@@ -23,9 +35,13 @@ export const cartSlice = createSlice({
           item.size !== size ||
           item.quantity !== quantity
       );
+
+      localStorage.setItem("cartState", JSON.stringify(state));
     },
     clearCart: (state) => {
       state.carts = [];
+
+      localStorage.setItem("cartState", JSON.stringify(state));
     },
   },
 });
