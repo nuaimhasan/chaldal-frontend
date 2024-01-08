0acb34a25f781e493d6526f4abb3624a6a19ea07
nuaimhasan <nuaim.emanager@gmail.com>
2024-01-08 19:05:14 +0600

    about us error slove

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-// import ContextProvider from "./ContextApi/ContextApi.jsx";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Provider } from "react-redux";
--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -5,21 +5,24 @@ import parcer from "html-react-parser";
 export default function AboutUs() {
   window.scroll(0, 0);
   const { data, isLoading } = useGetAboutQuery();
+  const about = data?.data[0];
+  const parcerDescription = about?.description && parcer(about?.description);
 
   if (isLoading) {
     return <Spinner />;
   }
 
-  const parcerDescription = parcer(data?.data?.description);
   return (
     <section className="py-5">
       <div className="container">
         <div className="grid md:grid-cols-2 gap-6 items-center">
           <div>
             <div className="w-max border-b-2 border-primary">
-              <h2 className="text-5xl font-bold">Welcome</h2>
+              <h2 className="text-5xl font-bold">{about?.title}</h2>
             </div>
-            <p className="text-neutral-content mt-2 text-lg">Aesthetic cloth</p>
+            <p className="text-neutral-content mt-2 text-lg">
+              {about?.subTitle}
+            </p>
 
             <div className="mt-4 text-neutral-content text-[15px]">
               <p>{parcerDescription}</p>
@@ -27,8 +30,8 @@ export default function AboutUs() {
           </div>
           <div className="hidden md:block">
             <img
-              src={`${import.meta.env.VITE_BACKEND_URL}/images/about/${
-                data?.data?.image
+              src={`${import.meta.env.VITE_BACKEND_URL}/aboutus/${
+                about?.image
               }`}
               alt=""
               className="w-[90%] mx-auto"
