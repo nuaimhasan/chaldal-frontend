6b2c17acd86239527ebaea43e4f9e9c86e9e932c
nasim0994 <nasimuddin0994@gmail.com>
2024-01-20 11:44:46 +0600

    rating star hover effect add

--- src/pages/ProductDetails/Review/ReviewModalForm.jsx
+++ src/pages/ProductDetails/Review/ReviewModalForm.jsx
@@ -1,25 +1,57 @@
 import { IoClose } from "react-icons/io5";
+import { FaStar } from "react-icons/fa";
+import { useState } from "react";
 
-export default function ReviewModalForm({modal,setModal}) {
+export default function ReviewModalForm({ modal, setModal }) {
+  const [currentValue, setCurrentValue] = useState(0);
+  const [hoverValue, setHoverValue] = useState(undefined);
+  const stars = Array(5).fill(0);
+
+
   return (
     <>
-      <button onClick={()=>setModal(false)} className={`modal_overlay ${modal && "modal_overlay_show"}`}></button>
-      <div className={`modal w-[95%] sm:w-[500px] p-4 ${modal && "modal_show"}`}>
+      <button
+        onClick={() => setModal(false)}
+        className={`modal_overlay ${modal && "modal_overlay_show"}`}
+      ></button>
+      <div
+        className={`modal w-[95%] sm:w-[500px] p-4 ${modal && "modal_show"}`}
+      >
         <div className="flex justify-between items-start">
           <div>
             <p>Product name</p>
             <p className="text-neutral-content text-sm">Rate this product</p>
           </div>
-          <button onClick={()=>setModal(false)} className="text-neutral-content hover:text-primary duration-200 text-lg">
+          <button
+            onClick={() => setModal(false)}
+            className="text-neutral-content hover:text-primary duration-200 text-lg"
+          >
             <IoClose />
           </button>
         </div>
 
         <div className="mt-4">
-          <div>
-            <input type="text" name="rating" className="border w-full rounded outline-none p-2 text-sm text-neutral" placeholder="your rating" />
+          <div className="flex items-center justify-center">
+            {stars.map((_, index) => {
+              return (
+                <FaStar
+                  key={index}
+                  size={20}
+                  onClick={() => setCurrentValue(index + 1)}
+                  onMouseOver={() => setHoverValue(index + 1)}
+                  onMouseLeave={()=>setHoverValue(undefined)}
+                  color={
+                    (hoverValue || currentValue) > index ? "#f47c20" : "#a9a9a9"
+                  }
+                  style={{
+                    marginRight: 10,
+                    cursor: "pointer",
+                  }}
+                />
+              );
+            })}
           </div>
-          <div className="mt-3">
+          <div className="mt-5">
             <textarea
               name=""
               rows="4"
